ffae3113ab3e22791b18427a0c3aa5ff26e918c8
151250111 <151250111@smail.nju.edu.cn>
2017-10-14 23:52:04 +0800

    developing: square

--- src/components/Square/newCard/newCard.js
+++ src/components/Square/newCard/newCard.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Card, Avatar } from 'antd';
+import style from './newCard.less';
+import defaultAvatar from '../../../assets/images/defaultAvatar.jpeg';
 
 function newCard(props) {
+  const avatar = props.info.avatarUrl === '' ?
+    defaultAvatar : props.info.avatarUrl;
   return (
-    <div>
-      card
-    </div>
+    <Card bordered={false} noHovering className={style['nc-wrap']}>
+      <div className={style['nc-userInfo-wrap']}>
+        <Avatar src={avatar} />
+        <span className={style['nc-userInfo-nickName']}>{props.info.nickName}</span>
+      </div>
+      <div className={style['nc-intro-wrap']}>
+        {props.info.intro}
+      </div>
+      <div className={style['nc-photo-wrap']}>
+        <img src={props.info.zipUrl} role="presentation" />
+      </div>
+      <div className={style['nc-footer']}>
+        <span>发表于{props.info.date}</span>
+      </div>
+    </Card>
   );
 }
+export default newCard;
--- src/routes/SquarePage/SquarePage.js
+++ src/routes/SquarePage/SquarePage.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { Layout } from 'antd';
+import { Layout, Row, Col, Card, Affix, Tabs } from 'antd';
 import { connect } from 'dva';
+import MediaQuery from 'react-responsive';
 import PixelHeader from '../../components/Pixel-Header/pixeliceHeader';
 import PixelFooter from '../../components/Pixel-Footer/pixelFooter';
 import Cover from '../../components/Cover/cover';
+import style from './SquarePage.less';
+import NewPhotoCard from '../../components/Square/newCard/newCard';
 
 const { Content } = Layout;
 
@@ -25,23 +28,140 @@ class SquarePage extends React.Component {
       photos: nextProps.photos,
     });
   }
+  smPane = () => {
+    const column1 = [];
+    const column2 = [];
+    for (let i = 0; i < this.state.photos.data.length; i += 2) {
+      const item1 = this.state.photos.data[i];
+      const item2 = this.state.photos.data[i + 1];
+      column1.push(
+        <NewPhotoCard key={i} info={item1} />,
+      );
+      if (item2 !== undefined) {
+        column2.push(
+          <NewPhotoCard key={i + 1} info={item2} />,
+        );
+      }
+    }
+    return ([
+      <Col span={12}>
+        {column1}
+      </Col>,
+      <Col span={12}>
+        {column2}
+      </Col>,
+    ]);
+  };
+  mdPane = () => {
+    const column1 = [];
+    const column2 = [];
+    const column3 = [];
+    for (let i = 0; i < this.state.photos.data.length; i += 3) {
+      const item1 = this.state.photos.data[i];
+      const item2 = this.state.photos.data[i + 1];
+      const item3 = this.state.photos.data[i + 2];
+      column1.push(
+        <NewPhotoCard key={i} info={item1} />,
+      );
+      if (item2 !== undefined) {
+        column2.push(
+          <NewPhotoCard key={i + 1} info={item2} />,
+        );
+      }
+      if (item3 !== undefined) {
+        column3.push(
+          <NewPhotoCard key={i + 2} info={item3} />,
+        );
+      }
+    }
+    return ([
+      <Col span={8}>
+        {column1}
+      </Col>,
+      <Col span={8}>
+        {column2}
+      </Col>,
+      <Col span={8}>
+        {column3}
+      </Col>,
+    ]);
+  };
+  xsPane = () => {
+    const column = [];
+    for (let i = 0; i < this.state.photos.data.length; i += 1) {
+      const item = this.state.photos.data[i];
+      column.push(
+        <NewPhotoCard key={i} info={item} />,
+      );
+    }
+    return (
+      <Col span={24}>
+        {column}
+      </Col>
+    );
+  };
+  paneCompute = (size) => {
+    let photos;
+    switch (size) {
+      case 'xs':
+        photos = this.xsPane();
+        break;
+      case 'sm':
+        photos = this.smPane();
+        break;
+      case 'md':
+        photos = this.mdPane();
+        break;
+      default:
+        photos = '';
+        break;
+    }
+    return photos;
+  };
   render() {
     const photos = [];
     for (let i = 0; i < this.state.photos.data.length; i += 1) {
       const item = this.state.photos.data[i];
       photos.push(
-        <div key={i}>
-          <img src={item.zipUrl} role="presentation" />
-        </div>,
+        <Col key={i}>
+          <NewPhotoCard info={item} />
+        </Col>,
       );
     }
     return (
       <Layout>
         <PixelHeader />
-        <Content>
+        <Content className="main-content">
           <Cover />
-          <div>
-            {photos}
+          <div className="content-wrap">
+            <Tabs defaultActiveKey="1">
+              <Tabs.TabPane tab="最新作品" key="1">
+                <div className={style['newphoto-wrap']}>
+                  <Row type="flex" justify="space-between" align="top">
+                    <Col xs={24} sm={20}>
+                      <Row type="flex" justify="start" align="top">
+                        <MediaQuery minWidth={992}>
+                          {this.paneCompute('md')}
+                        </MediaQuery>
+                        <MediaQuery minWidth={768} maxWidth={992}>
+                          {this.paneCompute('sm')}
+                        </MediaQuery>
+                        <MediaQuery maxWidth={768}>
+                          {this.paneCompute('xs')}
+                        </MediaQuery>
+                      </Row>
+                    </Col>
+                    <Col>
+                      <Affix>
+                        <Card span={4} >
+                          bla
+                        </Card>
+                      </Affix>
+                    </Col>
+                  </Row>
+                </div>
+              </Tabs.TabPane>
+            </Tabs>
           </div>
         </Content>
         <PixelFooter />
