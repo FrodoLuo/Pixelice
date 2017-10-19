0259813345cbce8520284a3ae3355ff7791c88fc
FrodoLuo <359303713@qq.com>
2017-10-19 19:56:05 +0800

    update

--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -11,7 +11,7 @@ function IndexPage() {
   return (
     <Layout>
       <PixeliceHeader home />
-      <Content className="main-content">
+      <Content className="main-content" style={{ padding: 0 }}>
         <Cover home author="No one" />
       </Content>
       <PixeliceFooter />
